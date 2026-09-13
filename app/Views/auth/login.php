4cffcadaca2b7ed0ff7a9c35de6216645acfa48f
ajiarya22 <ajiarya21@gmail.com>
2026-09-13 20:10:13 +0700

    Merubah Halaman login di Main

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -6,7 +6,7 @@
     <title>Login - Praktikum Acara 6</title>
 </head>
 <body>
-    <h2>Form Login</h2>
+    <h2>Form Login</h2><!-- Halaman Utama Login Sistem -->
 
     <!-- Tampilkan Flash Message jika ada -->
     <?php if (isset($_SESSION['flash'])): ?>
